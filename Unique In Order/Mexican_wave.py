ffddcd93e059004f43e122a3ff6a697b125f94a5
Atisam ul haq <38161450+atisamhaq123@users.noreply.github.com>
2023-05-19 16:50:17 +0500

    Delete Mexican_wave.py

--- Unique In Order/Mexican_wave.py
+++
@@ -1,9 +0,0 @@
-def unique_in_order(iterable):
-    new=[]
-    if len(iterable)>0:
-        for i in range(len(iterable)-1):
-            if iterable[i]!=iterable[i+1]:
-                new.append(iterable[i])
-        new.append(iterable[len(iterable)-1])
-        return new
-    return new
